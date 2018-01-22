77514708c2797a7f3084863f9bfd35803f0f6c14
Michael Smith <michaelsmith101@gmail.com>
2018-01-22 01:21:51 +0000

    sessions controller setup and tested

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -47,7 +47,12 @@ RSpec.describe UsersController, type: :controller do
             post :create, user: new_user_attributes
             expect(assigns(:user).password).to eq new_user_attributes[:password]
         end
-
+        
+        it "logs the user in after sign up" do
+           post :create, user: new_user_attributes
+           expect(session[:user_id]).to eq assigns(:user).id
+        end
+        
     end
     
 end
